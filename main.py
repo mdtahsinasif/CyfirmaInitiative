53870ef8bf4381b226aae5061643898ccd13f8d5
Md Tahsin Asif <32189971+mdtahsinasif@users.noreply.github.com>
2020-02-18 17:26:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,31 +6,6 @@ from openSourceRepo import OpenSourceRepo
 
 
 
-#
-# name = 'iana.org'
-# for qtype in 'A', 'AAAA', 'MX', 'NS', 'TXT', 'SOA':
-#     answer = dns.resolver.query(name,qtype, raise_on_no_answer=False)
-#     if answer.rrset is not None:
-#         print(answer.rrset)
-#
-#
-#
-# z = dns.zone.from_xfr(dns.query.xfr('nsztm1.digi.ninja', 'zonetransfer.me'))
-# names = z.nodes.keys()
-# sorted(names)
-# #names.sort
-# for n in names:
-#     print (z[n].to_text(n))
-
-
-
-#
-# from ipwhois import IPWhois
-#
-# ipInput = input('Enter Ip Adress: ')
-# obj = IPWhois(ipInput)
-# res=obj.lookup_whois()
-# print (res)
 
 
 if __name__ == '__main__':
