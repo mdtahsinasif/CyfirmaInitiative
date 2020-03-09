ac468d9f1bc052086b0a18c15293a6c40375782d
Md Tahsin Asif <32189971+mdtahsinasif@users.noreply.github.com>
2020-03-09 13:24:14 +0530

    updtades Main.py
    Putting switchcase

--- main.py
+++ main.py
@@ -1,15 +1,53 @@
 
-import dns.resolver
-import dns.query
-import dns.zone
+
 from openSourceRepo import OpenSourceRepo
+
+def week(choice):
+    switcher = {
+        1: one,
+        2: two
+    }
+    fun  = switcher.get(choice, "Invalid Choice")
+    print (fun())
+
+
+
+def one():
+    getOrgDetail()
+
+def two():
+    getIPDetails()
+
+
+
+
+
+def getOrgDetail():
+    orgName = input('Please enter the org name:')
+    OpenSourceRepo.openSourceDNDdata(orgName)
+
+
+def getIPDetails():
+    ipAddress = input('Please Enter the Ip Address:')
+
+    #     # 66.171.248.170
+    OpenSourceRepo.findIPDetails(ipAddress)
+
 
 
 
 
 
 if __name__ == '__main__':
-#    orgName = input('Please enter the org name:')
-    ipAddress = input('Please Enter the Ip Address:')
-#    OpenSourceRepo.openSourceDNDdata(orgName)
-    OpenSourceRepo.findIPDetails(ipAddress)
+    try:
+        choice = input("Enter Choice 1: for Organisation Details  Choice 2: for IP Address Details  ")
+        week(int(choice))
+    except:
+        print('Please enter the correct choice---------')
+
+
+
+
+
+
+
